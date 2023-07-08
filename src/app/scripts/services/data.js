ca9c241a7e636cf71fe3755123f4cf7ac2afa168
AnaRamirez12 <anamaria103458@gmail.com>
2023-07-07 22:30:28 -0500

    Updates

--- src/app/scripts/services/data.js
+++
@@ -1,6 +0,0 @@
-const URL_API = "https://backend-proyecto-final-sprint-1-modulo-2.onrender.com/";
-
-export const endpoints = {
-  urlUsers: `${URL_API}users`,
-  urlMessages: `${URL_API}messages`,
-};
